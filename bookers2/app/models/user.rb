29fc2bd63935953f4e26860051d8ee18b8210c63
EC2 Default User <ec2-user@ip-172-31-87-216.ec2.internal>
2021-12-09 05:54:31 +0000

    [add]応用4

--- bookers2/app/models/user.rb
+++ bookers2/app/models/user.rb
@@ -7,6 +7,12 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
   # has_manyはモデル名が複数形
   attachment :profile_image
 
@@ -16,4 +22,18 @@ class User < ApplicationRecord
   validates :introduction,length: { maximum: 50 }
     # 長さの下限を50文字に設定→minimum
     # presence: trueは、空欄でないことを確認している
+    
+    # フォローしたときの処理
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+    # フォローを外すときの処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+    # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+end
+    
 end
--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   devise_for :users
   root to: "homes#top"
   get "home/about" => "homes#about"
